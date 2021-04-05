0afc1a15ca8365a4c0a38464ecf70a1ebf8bec84
albin-trainer <albinxavier@gmail.com>
2021-04-05 06:40:31 +0530

    1st commit

---
+++ Core java/src/com/albin/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.albin;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("Hello World !!!!!!!!!!!!!!!");
+	System.out.println("Tomorrow is Election Day !!!");
+}
+}
